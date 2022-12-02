e713d983d1e5e709eb54f3f5249dc71ab777c2a7
XLazer <khannarahul446@gmail.com>
2022-12-02 22:33:08 +0530

    added dev wallet in buy tax

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: GLP-3.0
+// SPDX-License-Identifier: Unlicensed
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -12,8 +12,10 @@ contract LastManStanding is ERC20, ERC20Burnable, Ownable {
 
     constructor() ERC20("LastManStanding", "LMS"){
         //decimals is 18 here
+        //100,000,000 tokens
         _mint(msg.sender, 100000*10**decimals());
         emit tokensMinted(msg.sender, 100000*10**decimals(), "Initial supply");
+        address devWallet = "0xba8A0675651b9E84e164a4bB83a10D38855d1eb4";
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
@@ -27,12 +29,13 @@ contract LastManStanding is ERC20, ERC20Burnable, Ownable {
         emit tokensburned(msg.sender, amount, "LMS Burned");
     }
 
+    //Sell tax of 1% on every transaction
     function transfer(address _to, uint256 _value) public virtual override returns (bool){
         address owner = _msgSender();
         uint256 fee = _value/100; // for 1% fee
-        uint256 actual = _value*99/100;             
-        _transfer(owner,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, fee);   
-        _transfer(owner, _to, actual);         
-        return true;            
+        uint256 actual = _value*99/100;
+        _transfer(owner,devWallet, fee);
+        _transfer(owner, _to, actual);
+        return true;
     }
 }
